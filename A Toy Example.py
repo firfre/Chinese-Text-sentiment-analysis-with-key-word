621a0e505fda6c1818bc8700679a76f1f00d1e34
firfre <40306142+firfre@users.noreply.github.com>
2018-06-29 03:41:53 -0400

    Update A Toy Example.py

--- A Toy Example.py
+++ A Toy Example.py
@@ -8,6 +8,35 @@ Created on Wed Jun 27 15:24:25 2018
 
 
 #A Toy Example
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import jieba  
+import re
+import pickle
+from keras import backend as K
+from keras import initializers, regularizers, constraints
+from keras.engine import network
+from bs4 import BeautifulSoup
+import sys
+import os
+from keras.preprocessing import sequence
+from keras.models import Sequential,Model,load_model
+from keras.layers import Dense, Dropout, Embedding, LSTM, Bidirectional
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils.np_utils import to_categorical
+from keras.layers import Embedding, LSTM, GRU,  Input, Flatten, merge
+from keras.engine.topology import Layer, InputSpec
+from keras.layers import *
+from pprint import pprint
+from gensim import corpora
+from sklearn.cross_validation import train_test_split
+from collections import defaultdict
+from numpy.random import randint
+from numpy import argmax
+from keras.models import model_from_json
+from keras.callbacks import History 
 
 
 #pickle load data:
